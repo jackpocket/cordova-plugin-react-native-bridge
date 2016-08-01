b192f46405a75656726613ca752d03f442368a8a
Gabriel C. Troia <catalin.troia@gmail.com>
2016-08-01 16:26:37 -0400

    Add the right method name

--- src/ios/PG_2_RN_Bridge.m
+++ src/ios/PG_2_RN_Bridge.m
@@ -6,19 +6,21 @@
   // Member variables go here.
 }
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)invokeAcion:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation PG_2_RN_Bridge
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
+- (void)invokeAcion:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
 
     if (echo != nil && [echo length] > 0) {
+        NSLog(@"PG_2_RN_Bridge: invokeAcion Failed");
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
     } else {
+        NSLog(@"PG_2_RN_Bridge: invokeAcion Succeeded!");
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
 
