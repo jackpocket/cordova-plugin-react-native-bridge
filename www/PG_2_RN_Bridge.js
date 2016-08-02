360624ba21a5c5c383a1ec5e3b648096eae9c71d
Gabriel C. Troia <catalin.troia@gmail.com>
2016-08-01 20:02:37 -0400

    Add logs on the JS side on success and error cb

--- www/PG_2_RN_Bridge.js
+++ www/PG_2_RN_Bridge.js
@@ -1,10 +1,31 @@
 var exec = require('cordova/exec');
 
-exports.invokeAction = function(arg0, success, error) {
+exports.invokeAction = function (arg0, success, error) {
     // validate arg0 to the type
-    exec(success, error, "PG_2_RN_Bridge", "invokeAction", [arg0]);
+    exec(function () {
+        console.log('PG_2_RN_Bridge.invokeAction() success', arguments);
+        if (success) {
+            success.apply(success, arguments);
+        }
+    }, function (e) {
+        console.error('PG_2_RN_Bridge.invokeAction() error', arguments);
+        if (error) {
+            error.apply(success, arguments);
+        }
+    }, "PG_2_RN_Bridge", "invokeAction", [arg0]);
+
 };
 
-exports.revokeAction = function(arg0, success, error) {
-    exec(success, error, "PG_2_RN_Bridge", "revokeAction", []);
+exports.revokeAction = function (arg0, success, error) {
+    exec(function () {
+        console.log('PG_2_RN_Bridge.revokeAction() success', arguments);
+        if (success) {
+            success.apply(success, arguments);
+        }
+    }, function (e) {
+        console.error('PG_2_RN_Bridge.revokeAction() error', arguments);
+        if (error) {
+            error.apply(success, arguments);
+        }
+    }, "PG_2_RN_Bridge", "revokeAction", [null]);
 };
